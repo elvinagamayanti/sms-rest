f08cf800cb182f1920873f3ea389d016ec92025b
elvinagamayanti <pinaaae@gmail.com>
2025-07-25 03:01:30 +0700

    simpleactivitylogdto fixed

--- src/main/java/com/sms/controller/ActivityLogController.java
+++ src/main/java/com/sms/controller/ActivityLogController.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
